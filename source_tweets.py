af607e7586e76dbc99148c750153bb07a3aa6c3c
Samyuktha Madhusudhan Thumala <30373358+samyuktha17@users.noreply.github.com>
2020-07-14 21:18:43 -0400

    Update source_tweets.py

--- source_tweets.py
+++ source_tweets.py
@@ -4,17 +4,15 @@ from searchtweets import ResultStream
 import yaml
 import json
 
-def get_file(aname, cak, cask, etype, hashtag, keywords, fdate, tdate, ftime, ttime):
+def get_file(aname, cak, cask, etype, hashtag, keywords, fdate = '00-00-0000', tdate = '00-00-0000', ftime = '00:00', ttime = '00:00'):
     
-    label = aname
-    consumer_key = cak
-    consumer_secret = cask
-    if etype == 'efa': extraction = 'fullarchive'
-    
-    
-    endp = 'https://api.twitter.com/1.1/tweets/search/' + extraction + '/' + 'mobdata.json'
-    print(endp)
-    
+    if etype == 'efa': # Full archive scraping (refer to limits on README)
+        endp = 'https://api.twitter.com/1.1/tweets/search/fullarchive/' + aname + '.json'
+    elif etype == 'tdays': # 30 days scraping (refer to limits on README)
+        endp = 'https://api.twitter.com/1.1/tweets/search/30day/' + aname + '.json'
+    else:
+        endp = 'ERROR'
+     
     # Creating a yaml credentials file
     config = dict(
     search_tweets_api = dict(
@@ -33,30 +31,25 @@ def get_file(aname, cak, cask, etype, hashtag, keywords, fdate, tdate, ftime, tt
                                        yaml_key = 'search_tweets_api',
                                        env_overwrite = True)
     print(premium_search_args)
-    print(fdate)
-    rule = gen_rule_payload(results_per_call = 100,
-                            from_date = fdate + ' '+ ftime, #"2019-07-06 01:00",
-                            to_date = tdate + ' ' + ttime, #"2019-07-06 02:15",
-                            pt_rule = keywords,
-                            )
+    
+    if etype == 'efa':
+        rule = gen_rule_payload(results_per_call = 100,
+                                from_date = fdate + ' '+ ftime, #"2019-07-06 01:00",
+                                to_date = tdate + ' ' + ttime, #"2019-07-06 02:15",
+                                pt_rule = keywords,
+                                )
+    else:
+        rule = gen_rule_payload(results_per_call = 100,
+                               pt_rule = keywords)
+            
     
     # result stream
-    
+
     rs = ResultStream(rule_payload = rule,
-                      max_results = 10,
+                      max_results = 50,
                       **premium_search_args)
     
 
-    # save file 
-    """
-    with open('C:\\Users\\Samuktha\\Documents\\USC\\twitter\\proj\\data.json','a',encoding = 'utf-8') as f:
-        for tweet in rs.stream():
-            #print('{0}: {1}'.format(str(n), tweet['created_at']))
-            json.dump(tweet, f)
-            f.write('\n')
-
-    print('done')
     
-    """
     
     return rs
